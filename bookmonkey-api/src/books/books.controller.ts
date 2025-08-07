5d5cda85e190d272674fdaaf3967e4291b762b30
CSchulz <kontakt@schulzolutions.de>
2025-08-07 13:49:09 +0200

    add books.service.ts

--- bookmonkey-api/src/books/books.controller.ts
+++ bookmonkey-api/src/books/books.controller.ts
@@ -25,99 +25,47 @@ import { CreateBookDto } from './dto/create-book.dto';
 import { BuyBookDto } from './dto/buy-book.dto';
 import { DomainException } from './exceptions/domain.exception';
 import { RatingBookDto } from './dto/rating-book.dto';
+import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
-
-  private books: BookEntity[] = [
-    new BookEntity({
-      id: randomUUID(),
-      isbn: '978-0061976209',
-      title: 'The Whale',
-      authors: ['Samuel D. Hunter'],
-      price: 16.99,
-      amount: 1000,
-    }),
-    new BookEntity({
-      id: randomUUID(),
-      isbn: '978-0061976209',
-      title: 'The Whale',
-      authors: ['Samuel D. Hunter'],
-      price: 16.99,
-      amount: 1000,
-    }),
-  ];
+  constructor(protected booksService: BooksService) {}
 
   @Get()
   @ApiOkResponse({ description: 'OK', type: BookEntity, isArray: true })
   getAll() {
-    return this.books;
+    return this.booksService.findAll();
   }
 
   @Get('find')
   @ApiOkResponse({ description: 'OK', type: BookEntity })
   @ApiNotFoundResponse()
   find(@Query('isbn') isbn: string) {
-    return this.findBookByPredicate((book) => book.isbn === isbn);
+    return this.booksService.findBookByQueryParams(isbn);
   }
 
   @Get(':id')
   @ApiOkResponse({ description: 'OK', type: BookEntity })
   @ApiNotFoundResponse()
   findById(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.findBookByPredicate((book) => book.id === id);
+    return this.booksService.findOne(id);
   }
 
   @Post()
   @ApiCreatedResponse({ description: 'Book created', type: BookEntity })
   createBook(@Body() createBookDto: CreateBookDto) {
-    const newBook = new BookEntity({
-      id: randomUUID(),
-      title: createBookDto.title,
-      isbn: createBookDto.isbn,
-      authors: createBookDto.authors,
-      amount: 0,
-      price: 0,
-    });
-
-    this.books.push(newBook);
-
-    return newBook;
+    return this.booksService.create(createBookDto);
   }
 
   @Put(':id/buy')
   @HttpCode(204)
   buy(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: BuyBookDto) {
-    const book = this.findBookByPredicate((book) => book.id === id);
-
-    if (book.amount - body.amount < 0) {
-      throw new DomainException(
-        `Order cancelled: ${body.amount} copys of "${book.title}" have been ordered, but only ${book.amount} are available. More will be available, soon!`
-      );
-    }
-
-    book.amount = book.amount - body.amount;
+    this.booksService.buy(id, body);
   }
 
   @Put(':id/rating')
   @HttpCode(204)
   addRating(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: RatingBookDto) {
-    const book = this.findBookByPredicate((book) => book.id === id);
-
-    if (book.rating == 0) {
-      book.rating = body.rating;
-    } else {
-      book.rating = (book.rating + body.rating) / 2
-    }
-  }
-
-  private findBookByPredicate(predicate: (book: BookEntity) => boolean) {
-    const book = this.books.find((book) => predicate(book));
-
-    if (!book) {
-      throw new NotFoundException();
-    } else {
-      return book;
-    }
+    this.booksService.addRating(id, body);
   }
 }
---
+++ bookmonkey-api/src/books/books.service.ts
@@ -0,0 +1,88 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BookEntity } from './entities/book.entity';
+import { randomUUID } from 'crypto';
+import { CreateBookDto } from './dto/create-book.dto';
+import { BuyBookDto } from './dto/buy-book.dto';
+import { RatingBookDto } from './dto/rating-book.dto';
+import { DomainException } from './exceptions/domain.exception';
+
+@Injectable()
+export class BooksService {
+    private books: BookEntity[] = [
+      new BookEntity({
+        id: randomUUID(),
+        isbn: '978-0061976209',
+        title: 'The Whale',
+        authors: ['Samuel D. Hunter'],
+        price: 16.99,
+        amount: 1000,
+      }),
+      new BookEntity({
+        id: randomUUID(),
+        isbn: '978-0061976209',
+        title: 'The Whale',
+        authors: ['Samuel D. Hunter'],
+        price: 16.99,
+        amount: 1000,
+      }),
+    ];
+
+    findAll() {
+        return this.books;
+    }
+
+    findBookByQueryParams(isbn: string) {
+        return this.findBookByPredicate((book) => book.isbn === isbn)
+    }
+
+    findOne(id: string) {
+        return this.findBookByPredicate((book) => book.id === id)
+    }
+
+    create(createBookDto: CreateBookDto) {
+        const newBook = new BookEntity({
+            id: randomUUID(),
+            title: createBookDto.title,
+            isbn: createBookDto.isbn,
+            authors: createBookDto.authors,
+            amount: 0,
+            price: 0,
+          });
+      
+          this.books.push(newBook);
+      
+          return newBook;
+    }
+
+    buy(id: string, body: BuyBookDto) {
+        const book = this.findBookByPredicate((book) => book.id === id);
+
+        if (book.amount - body.amount < 0) {
+        throw new DomainException(
+            `Order cancelled: ${body.amount} copys of "${book.title}" have been ordered, but only ${book.amount} are available. More will be available, soon!`
+        );
+        }
+
+        book.amount = book.amount - body.amount;
+    }
+
+    addRating(id: string, body: RatingBookDto) {
+        const book = this.findBookByPredicate((book) => book.id === id);
+
+        if (book.rating == 0) {
+        book.rating = body.rating;
+        } else {
+        book.rating = (book.rating + body.rating) / 2
+        }
+    }
+
+    private findBookByPredicate(predicate: (book: BookEntity) => boolean) {
+      const book = this.books.find((book) => predicate(book));
+  
+      if (!book) {
+        throw new NotFoundException();
+      } else {
+        return book;
+      }
+    }
+}
